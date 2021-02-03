7ebb04720cc0f1dde2a8042dc21bd2e3629f706f
beni69 <73666440+beni69@users.noreply.github.com>
2021-02-03 21:46:09 +0100

    colors done with hex and rgb

--- api.js
+++ api.js
@@ -1,26 +1,48 @@
 const express = require("express");
 const app = express();
 const light = require("./light");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/light", (req, res, next) => {
-    res.json({data: "Hello World!"});
+app.get("/", (req, res) => {
+    succ(res, "Hello World!");
 });
 
-app.post("/light/toggle", (req, res, next) => {
+app.post("/toggle", (req, res) => {
     light.toggle();
-    res.json({data: "Success!"});
+    succ(res);
 });
 
-app.post("/light/on", (req, res, next) => {
+app.post("/on", (req, res) => {
     light.on();
     res.json({data: "Success!"});
 });
 
-app.post("/light/off", (req, res, next) => {
+app.post("/off", (req, res) => {
     light.off();
     res.json({data: "Success!"});
+});
+
+app.post("/color", (req, res) => {
+    const color = req.body.color || req.query.color;
+    if (!color) return err(res, "No color provided.");
+    const type = req.body.type || req.query.type;
+    if (!type) return err(res, "No color type provided.");
+    light.color(res, color, type);
+    succ(res);
 });
 
 app.listen(3333, () => {
     console.log("Server ready!");
 });
+
+//*
+function succ(res, msg = "Success!") {
+    res.json({data: msg});
+}
+function err(res, msg, code = 400) {
+    res.status(code).json({data: "Error!", error: msg});
+}
+
+module.exports = {succ: succ, err: err};
--- light.js
+++ light.js
@@ -1,4 +1,5 @@
 const Yeelight = require("yeelight2");
+const api = require("./api");
 module.exports = {
     toggle: () => {
         Promise.resolve()
@@ -17,6 +18,7 @@ module.exports = {
             })
             .then(sleep());
     },
+
     on: () => {
         Promise.resolve()
             .then(
@@ -31,8 +33,10 @@ module.exports = {
             .then(light => {
                 light.set_power("on").then(console.log("Power on succeeded"));
                 return light;
-            });
+            })
+            .then(sleep());
     },
+
     off: () => {
         Promise.resolve()
             .then(
@@ -47,7 +51,31 @@ module.exports = {
             .then(light => {
                 light.set_power("off").then(console.log("Power off succeeded"));
                 return light;
-            });
+            })
+            .then(sleep());
+    },
+
+    color: (res, color, type) => {
+        Promise.resolve()
+            .then(
+                () =>
+                    new Promise(accept => {
+                        Yeelight.discover(function (light) {
+                            this.close();
+                            accept(light);
+                        });
+                    })
+            )
+            .then(light => {
+                // console.log(color);
+                if (type == "hex")
+                    light.set_rgb("0x" + color.replace(/0x|#/, ""));
+                else if (type == "rgb")
+                    if (toHexCol(color) != null) light.set_rgb(toHexCol(color));
+                // TODO: color names
+                return light;
+            })
+            .then(sleep());
     },
 };
 
@@ -56,4 +84,16 @@ function sleep(time) {
     return function (o) {
         return new Promise(fn => setTimeout(() => fn(o), time));
     };
+}
+
+function toHexNum(input) {
+    let hex = Number(input.trim()).toString(16);
+    if (hex.length < 2) hex = "0" + hex;
+    return hex;
+}
+// const toHexNum = rgb => rgb.toString(16).length < 2 ? "0" + rgb.toString(16) : rgb.toString(16);
+function toHexCol(str) {
+    const x = str.replace("#", "").split(/\s|,/);
+    if (x.length != 3) return null;
+    return "0x" + toHexNum(x[0]) + toHexNum(x[1]) + toHexNum(x[2]);
 }
