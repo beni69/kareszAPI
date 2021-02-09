5355490a43dfe9d4150eeec4953b8150d34415cd
beni69 <73666440+beni69@users.noreply.github.com>
2021-02-09 20:21:10 +0100

    cors moment

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // frameworks
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // setup
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 
 // modules
 require("./modules/light.js")(app, succ, err);
