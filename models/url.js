9a0880437ba48d1d5222401ef09c3b2907adbf22
beni69 <73666440+beni69@users.noreply.github.com>
2021-02-17 09:46:56 +0100

    _ _ moment

--- models/url.js
+++ models/url.js
@@ -3,7 +3,7 @@ module.exports = mongoose.model(
     "url",
     new mongoose.Schema({
         _id: String,
-        _v: {type: Number, default: 0},
+        __v: {type: Number, default: 0},
         url: String,
         dest: String,
         clicks: {type: Number, default: 0},
