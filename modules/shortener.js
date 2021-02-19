4e23c5e1757337354e0301fb70c1517850c2cf75
beni69 <73666440+beni69@users.noreply.github.com>
2021-02-19 13:55:44 +0100

    asd

--- modules/shortener.js
+++ modules/shortener.js
@@ -74,7 +74,7 @@ router.get("/shortener", async (req, res) => {
 
 // delete a link
 router.delete("/shortener", async (req, res) => {
-    const code = req.body.data || req.query.data || null;
+    const code = req.query.code || null;
     if (code == null) return err(res, "Please provide a url or a code");
 
     // get the entry by either the url or the code
