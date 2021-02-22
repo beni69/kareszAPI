1e0509a5135d3dfae06c4e20002b309741f004c0
beni69 <73666440+beni69@users.noreply.github.com>
2021-02-22 13:02:15 +0100

    part one done!
    / for the entire table, /day, /now, /next

--- modules/timetable.js
+++ modules/timetable.js
@@ -4,14 +4,29 @@ const config = require("config");
 const { succ, err } = require("./index");
 const fs = require("fs");
 const jsonc = require("jsonc");
+const file = fs.readFileSync("./config/timetable.jsonc").toString();
+const table = jsonc.parse(file);
 
 router.get("/", (req, res) => {
-    const file = fs.readFileSync("./config/timetable.jsonc").toString();
-    const json = jsonc.parse(file);
+    res.json(table);
+});
 
-    console.log(json);
-    res.json(json);
-    // res.json(require("../config/timetable.jsonc"));
+router.get("/day", (req, res) => {
+    res.json(table[new Date().getDay()]);
+});
+
+router.get("/now", (req, res) => {
+    const now = new Date();
+    const day = table[now.getDay()];
+    const p = now.getHours() - 8;
+    res.json(day[p]);
+});
+
+router.get("/next", (req, res) => {
+    const now = new Date();
+    const day = table[now.getDay()];
+    const p = now.getHours() - 7;
+    res.json(day[p]);
 });
 
 module.exports = router;
